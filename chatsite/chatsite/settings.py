ec582e466f34b887010a3e096a3e550e64a86717
root <root@ip-172-31-22-99.ec2.internal>
2019-05-21 04:51:11 +0000

    Chat server with Channels now working

--- chatsite/chatsite/settings.py
+++ chatsite/chatsite/settings.py
@@ -131,7 +131,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [('redis', 6379)],
         },
         "Routing": "chatsite.routing.application",
     },
